2d920f993709405a555c6af68a1eab1f2565ae5b
Poorin Pichayamongkol <poorinsmoothy@hotmail.com>
2017-03-03 09:40:57 +0700

    Exercise

---
+++ codeguide/Ex01.java
@@ -0,0 +1,27 @@
+package codeguide;
+
+public class Ex01 {
+	public void onPCGame() { 
+	    // turn on game console
+	    System.out.println("PC game is on");
+	    // play game
+	    System.out.println("the game is playing");
+	    // stop play game
+	    System.out.println("game is stopped");
+	    //turn off game console
+	    System.out.println("game console is off");
+	} 
+	
+	public void onPSGame() { 
+	    // turn on game console
+	    System.out.println("PS game is on");
+	    // play game
+	    System.out.println("the game is playing");
+	    // stop play game
+	    System.out.println("game is stopped");
+	    //turn off game console
+	    System.out.println("game console is off");
+	}
+	
+	//TODO-write the method(s) to solved DRY(don't repeat yourself) code.
+}
---
+++ codeguide/Ex02.java
@@ -0,0 +1,26 @@
+package codeguide;
+
+public class Ex02 {
+	public void hangOut(){
+		System.out.println("wake up");
+		System.out.println("take a bath");
+		System.out.println("wear the clothes");
+		System.out.println("open the door");
+		System.out.println("close the door");
+		System.out.println("walk");
+	}
+	
+	//TODO fill the statements in the blank methods.
+	public void wakeUp(){
+	//fill it 
+		
+	}
+	
+	public void takeABath(){
+	//fill it
+		
+	}
+	//TODO continue coding that way to complete and study how to code 
+	//the method that need to have one function in each method.
+	// After that write method main and test your method .
+}
